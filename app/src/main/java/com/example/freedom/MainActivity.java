deaf309d79bd50c101eb77924e72187f11806264
yogeshbhatia677 <yogeshbhatia677@gmail.com>
2020-02-21 22:19:49 +0530

    button added in main activity

--- app/src/main/java/com/example/freedom/MainActivity.java
+++ app/src/main/java/com/example/freedom/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.freedom;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button1;
 
 
     @Override
@@ -12,6 +17,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View view) {
+                                           openHomeActivity();
+
+                                       }
+                                   }
+
+        );
+
     }
+
+    public void openHomeActivity() {
+
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
 }
 
