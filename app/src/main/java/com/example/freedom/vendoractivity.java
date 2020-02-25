4b32b0631b26165e0ba597136d7cfa177017ac88
yogeshbhatia677 <yogeshbhatia677@gmail.com>
2020-02-25 20:37:32 +0530

    vendor activity 1

---
+++ app/src/main/java/com/example/freedom/vendoractivity.java
@@ -0,0 +1,14 @@
+package com.example.freedom;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class vendoractivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vendoractivity);
+    }
+}
